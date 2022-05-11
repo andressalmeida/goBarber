5d648f6d291f751d2992e1d29407097244c30475
Andressa Almeida <andressa.vdk93@gmail.com>
2022-05-11 13:03:21 -0300

    autenticação de rotas

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,22 +1,32 @@
-import {Request, Response, NextFunction } from 'express';
+import {Request, Response, NextFunction, request } from 'express';
 import {verify} from 'jsonwebtoken';
 import authConfig from '../config/auth'
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
 
 export default function ensureAuthenticated(req: Request, res: Response, next: NextFunction): void {
 
     const authHeader = req.headers.authorization;
-
+  
     if(!authHeader) {
         throw new Error('JWT token is missing')
     }
 
-    const [, token] = authHeader.split('');
-
+    const [, token] = authHeader.split(' ');
+ 
     try {
         const decoded = verify(token, authConfig.jwt.secret)
-
-        console.log(decoded)
         
+        const { sub } = decoded as TokenPayload;
+
+        request.user = {
+            id: sub,
+        }
+
         return next()
     } catch {
         throw new Error('Invalid JWT token');
